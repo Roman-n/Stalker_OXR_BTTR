bbe1b2be4572b1b0999ab72822959b5a5986f3b8
oldSerpskiStalker <ictopiar@gmail.com>
2019-08-01 01:22:44 +0500

    *Да бда я, затупил, прописал инерцию прицела в DEBUG.(Автор фишки: v2v3v4 - XRay Oxygen)

--- src/xrGame/HUDCrosshair.cpp
+++ src/xrGame/HUDCrosshair.cpp
@@ -58,6 +58,56 @@ BOOL g_bDrawFirstBulletCrosshair = FALSE;
 
 void CHUDCrosshair::OnRenderFirstBulletDispertion()
 {
+	VERIFY			(g_bRendering);
+	Fvector2		center;
+	Fvector2		scr_size;
+	scr_size.set	(float(::Render->getTarget()->get_width()), float(::Render->getTarget()->get_height()));
+	center.set		(scr_size.x/2.0f, scr_size.y/2.0f);
+
+	UIRender->StartPrimitive		(10, IUIRender::ptLineList, UI().m_currentPointType);
+
+	u32	fb_cross_color				= color_rgba(255, 0, 0, 255); //red
+	
+
+	float cross_length				= /*cross_length_perc*/0.008f*scr_size.x;
+	float min_radius				= min_radius_perc*scr_size.x;
+	float max_radius				= max_radius_perc*scr_size.x;
+
+	clamp							(target_radius , min_radius, max_radius);
+
+	float x_min						= min_radius + fb_radius;
+	float x_max						= x_min + cross_length;
+
+	float y_min						= x_min;
+	float y_max						= x_max;
+
+	// 0
+	UIRender->PushPoint(center.x,			center.y + y_min,	0, fb_cross_color, 0,0);
+	UIRender->PushPoint(center.x,			center.y + y_max,	0, fb_cross_color, 0,0);
+	// 1
+	UIRender->PushPoint(center.x,			center.y - y_min,	0, fb_cross_color, 0,0);
+	UIRender->PushPoint(center.x,			center.y - y_max,	0, fb_cross_color, 0,0);
+	// 2
+	UIRender->PushPoint(center.x + x_min,	center.y,			0, fb_cross_color, 0,0);
+	UIRender->PushPoint(center.x + x_max,	center.y,			0, fb_cross_color, 0,0);
+	// 3
+	UIRender->PushPoint(center.x - x_min,	center.y,			0, fb_cross_color, 0,0);
+	UIRender->PushPoint(center.x - x_max,	center.y,			0, fb_cross_color, 0,0);
+	
+	// point
+	UIRender->PushPoint(center.x-0.5f,		center.y,			0, fb_cross_color, 0,0);
+	UIRender->PushPoint(center.x+0.5f,		center.y,			0, fb_cross_color, 0,0);
+
+
+	// render	
+	UIRender->SetShader						(*hShader);
+	UIRender->FlushPrimitive				();
+}
+#endif
+
+extern ENGINE_API BOOL g_bRendering;
+void CHUDCrosshair::OnRender()
+{
     VERIFY(g_bRendering);
 	
     Fvector2		center, scr_size;
@@ -92,54 +142,6 @@ void CHUDCrosshair::OnRenderFirstBulletDispertion()
 		center.set		(x, y);
 	else
 		center.set(scr_size.x / 2.0f, scr_size.y / 2.0f);
-
-    GEnv.UIRender->StartPrimitive(10, IUIRender::ptLineList, UI().m_currentPointType);
-
-    u32 fb_cross_color = color_rgba(255, 0, 0, 255); // red
-
-    float cross_length = /*cross_length_perc*/ 0.008f * scr_size.x;
-    float min_radius = min_radius_perc * scr_size.x;
-    float max_radius = max_radius_perc * scr_size.x;
-
-    clamp(target_radius, min_radius, max_radius);
-
-    float x_min = min_radius + fb_radius;
-    float x_max = x_min + cross_length;
-
-    float y_min = x_min;
-    float y_max = x_max;
-
-    // 0
-    GEnv.UIRender->PushPoint(center.x, center.y + y_min, 0, fb_cross_color, 0, 0);
-    GEnv.UIRender->PushPoint(center.x, center.y + y_max, 0, fb_cross_color, 0, 0);
-    // 1
-    GEnv.UIRender->PushPoint(center.x, center.y - y_min, 0, fb_cross_color, 0, 0);
-    GEnv.UIRender->PushPoint(center.x, center.y - y_max, 0, fb_cross_color, 0, 0);
-    // 2
-    GEnv.UIRender->PushPoint(center.x + x_min, center.y, 0, fb_cross_color, 0, 0);
-    GEnv.UIRender->PushPoint(center.x + x_max, center.y, 0, fb_cross_color, 0, 0);
-    // 3
-    GEnv.UIRender->PushPoint(center.x - x_min, center.y, 0, fb_cross_color, 0, 0);
-    GEnv.UIRender->PushPoint(center.x - x_max, center.y, 0, fb_cross_color, 0, 0);
-
-    // point
-    GEnv.UIRender->PushPoint(center.x - 0.5f, center.y, 0, fb_cross_color, 0, 0);
-    GEnv.UIRender->PushPoint(center.x + 0.5f, center.y, 0, fb_cross_color, 0, 0);
-
-    // render
-    GEnv.UIRender->SetShader(*hShader);
-    GEnv.UIRender->FlushPrimitive();
-}
-#endif
-
-extern ENGINE_API BOOL g_bRendering;
-void CHUDCrosshair::OnRender()
-{
-    VERIFY(g_bRendering);
-    Fvector2 center;
-    Fvector2 scr_size;
-    scr_size.set(float(GEnv.Render->getTarget()->get_width()), float(GEnv.Render->getTarget()->get_height()));
-    center.set(scr_size.x / 2.0f, scr_size.y / 2.0f);
 
     GEnv.UIRender->StartPrimitive(10, IUIRender::ptLineList, UI().m_currentPointType);
 
