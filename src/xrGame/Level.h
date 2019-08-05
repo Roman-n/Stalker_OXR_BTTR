358691fda29c8d9e9230ea987bbfe832383d84d6
oldSerpskiStalker <ictopiar@gmail.com>
2019-08-05 15:43:06 +0500

    Revert "*Remove mp code (OXR_COC: qweasdd136963)"
    This reverts commit fada90d93b654c3993c5f03486c4f64b0fbfc588.

--- src/xrGame/Level.h
+++ src/xrGame/Level.h
@@ -241,6 +241,7 @@ protected:
     bool xr_stdcall net_start_client4();
     bool xr_stdcall net_start_client5();
     bool xr_stdcall net_start_client6();
+    void net_OnChangeSelfName(NET_Packet* P);
     void CalculateLevelCrc32();
 
 public:
@@ -252,6 +253,8 @@ public:
     shared_str m_caClientOptions;
     // Starting/Loading
     virtual bool net_Start(const char* op_server, const char* op_client);
+    virtual void net_Load(const char* name);
+    virtual void net_Save(const char* name);
     virtual void net_Stop();
     virtual bool net_Start_client(const char* name);
     virtual void net_Update();
